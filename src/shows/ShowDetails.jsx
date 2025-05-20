4f1146534db8acc42a1e553150e629b15ac20ff2
Stacy Lee <stacy.lee92@gmail.com>
2025-05-20 15:32:51 -0700

    complete

--- src/shows/ShowDetails.jsx
+++ src/shows/ShowDetails.jsx
@@ -1,6 +1,24 @@
 import "./shows.css";
+import { useState } from "react";
+import EpisodeList from "../episodes/EpisodeList";
+import EpisodeDetails from "../episodes/EpisodeDetails";
 
 /** Allows users to browse through the episodes of the given show */
-export default function ShowDetails() {
-  return <div className="show-details"></div>;
+export default function ShowDetails({ show }) {
+  const [selectedEpisode, setSelectedEpisode] = useState();
+  if (!show) {
+    return <p>Please select a show.</p>;
+  }
+
+  return (
+    <div className="show-details">
+      <EpisodeList
+        name={show.name}
+        episodes={show.episodes}
+        selectedEpisode={selectedEpisode}
+        setSelectedEpisode={setSelectedEpisode}
+      />
+      <EpisodeDetails episode={selectedEpisode} />
+    </div>
+  );
 }
